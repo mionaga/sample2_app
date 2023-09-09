769a0d91298e89855ebae7a3da7062c004927c7f
mionaga <143914849+mionaga@users.noreply.github.com>
2023-09-09 11:41:35 +0000

    app-2

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'lists/new'
+  get 'lists/index'
+  get 'lists/show'
+  post 'lists' => 'lists#create'
+  get 'listes/edit'
+  get '/top'=>'homes#top'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
